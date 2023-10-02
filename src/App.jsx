d7606fe55b5bea97d89fb1e3f5be50c3343eb915
AngelBarrios <angelefrenbarriosromero234@gmail.com>
2023-10-02 18:19:38 -0300

    agregando itemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Navbar } from "./components/layout/navbar/Navbar";
+import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ItemListContainer />
     </>
   );
 }
---
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+const ItemListContainer = () => {
+  return (
+    <div className="container">
+      <h1>Bienvenido a la pagina principal</h1>
+      <h2>Todos los juguetes que te puedas imaginar y más</h2>
+    </div>
+  );
+};
+
+export default ItemListContainer;
