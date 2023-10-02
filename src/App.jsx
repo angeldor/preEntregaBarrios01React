1e8d924dd967f819d4930d0c267a4f6337d5c923
AngelBarrios <angelefrenbarriosromero234@gmail.com>
2023-10-02 20:10:48 -0300

    agregando props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { Navbar } from "./components/layout/navbar/Navbar";
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 
 function App() {
+  const titulo = "Bienvendido a nuestra tienda de juguetes!";
   return (
     <>
       <Navbar />
-      <ItemListContainer />
+      <ItemListContainer titulo={titulo} />
     </>
   );
 }
--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,10 +1,10 @@
 import "./ItemListContainer.css";
 import React from "react";
 
-const ItemListContainer = () => {
+const ItemListContainer = (props) => {
   return (
     <div className="container">
-      <h2>Bienvenido a la pagina principal</h2>
+      <h2>{props.titulo}</h2>
       <p>Todos los juguetes que te puedas imaginar y más...</p>
     </div>
   );
